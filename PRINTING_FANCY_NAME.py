b558d721977fd17739e9ddf159c792430b1d8cb6
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-03-16 17:57:58 +0700

    Inputting the main program

--- PRINTING_FANCY_NAME.py
+++ PRINTING_FANCY_NAME.py
@@ -1,5 +1,14 @@
 # Main program
+if __name__ == "__main__":
+    
     # Prompt user to input their name
+    name = input("Enter your name: ")
+    
     # Prompt user to input their dream job
-    # Print name 
-    # Print dream job 
+    dream_job = input("Enter your dream job: ")
+    
+    # Print name in rainbow colors
+    print("\nYour name:", name)
+    
+    # Print dream job in rainbow colors
+    print("\nYour dream job: ", dream_job)
